4a372593cefb14b2c2e65487b4142f11e58070d6
DuckGoesQuack <samokat104@gmail.com>
2015-03-07 19:19:03 +0500

    1

---
+++ forest/ConsoleApplication1/ConsoleApplication1/Creature.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Creature
+    {
+        public char Type;
+        public int X;
+        public int Y;
+        public int Lifes;
+        public string Name;
+        public Creature(char type, string name, int x, int y, int l = 1)
+        {
+            Type = type;
+            X = x;
+            Y = y;
+            Lifes = l;
+            Name = name; 
+        }
+
+        public bool IsAlive()
+        {
+            
+            return Lifes > 0;
+        }
+    }
+}
---
+++ forest/ConsoleApplication1/ConsoleApplication1/Direction.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public abstract class Direction
+    {
+        public virtual int dx
+        {
+            get { return 0; }
+        }
+
+        public virtual int dy
+        {
+            get { return 0; }
+        }
+    }
+
+    public class Up : Direction
+    {
+        public override int dx
+        {
+            get { return -1; }
+        }
+    }
+    public class Down : Direction
+    {
+        public override int dx
+        {
+            get { return 1; }
+        }
+
+    }
+    public class Left : Direction
+    {
+        public override int dy
+        {
+            get { return -1; }
+        }
+    }
+    public class Right : Direction
+    {
+        public override int dy
+        {
+            get { return 1; }
+        }
+    }
+}
